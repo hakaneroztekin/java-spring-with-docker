33dc5e1a743f53ad888f42bb87ba1b54403b6be3
hakan.eroztekin <hakan.eroztekin@trendyol.com>
2022-06-17 10:37:29 +0300

    init spring app

---
+++ src/main/java/com/hakaneroztekin/javaspringwithdocker/JavaSpringWithDockerApplication.java
@@ -0,0 +1,13 @@
+package com.hakaneroztekin.javaspringwithdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWithDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hakaneroztekin/javaspringwithdocker/JavaSpringWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakaneroztekin.javaspringwithdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
